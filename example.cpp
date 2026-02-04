e4899953cb46e808358efc39d2b7e582c01b8eb6
suhyeon <sinsu321@gmail.com>
2026-02-05 00:18:44 +0900

    readme 수정 & 하드웨어

--- example.cpp
+++ example.cpp
@@ -3,6 +3,6 @@
 int main(int /* argc */, char** /* argv */) {
     SoundCapturing sound_capturing;
     // sound_capturing.process("default");
-    sound_capturing.process("hw:1,0");
-
+    // sound_capturing.process("hw:1,0");
+    sound_capturing.process("plughw:1,0");
 }
